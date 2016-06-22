9e076e944e27b8677f1785989698a8d859849733
Martin Carbonneau <martinusca@gmail.com>
2016-06-22 20:26:31 +0200

    feature/esp8266 first step, light on and light off implemented

--- mycroft/skills/esp8266/__init__.py
+++ mycroft/skills/esp8266/__init__.py
@@ -21,6 +21,42 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
+from urllib2 import urlopen
+
 __author__ = 'Dark5ide'
 
 LOGGER = getLogger(__name__)
+
+class Esp8266Skill(MycroftSkill):
+
+    def __init__(self):
+        super(Esp8266Skill, self).__init__(name="Esp8266Skill")
+    
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+        self.__build_turn_on_lights()
+        self.__build_turn_off_lights()
+        
+    def __build_turn_on_lights(self):
+        intent = IntentBuilder("TurnOnAllLightsIntent").require("TurnOnLightsKeyword").build()
+        self.register_intent(intent, self.handle_turn_on_lights)
+        
+    def __build_turn_off_lights(self):
+        intent = IntentBuilder("TurnOffAllLightsIntent").require("TurnOffLightsKeyword").build()
+        self.register_intent(intent, self.handle_turn_off_lights)
+        
+    def handle_turn_on_lights(self, message):
+        urlopen("http://esp8266.local/led0?state=on")
+        urlopen("http://esp8266.local/led1?state=on")
+        self.speak_dialog("lights.on")
+        
+    def handle_turn_off_lights(self, message):
+        urlopen("http://esp8266.local/led0?state=off")
+        urlopen("http://esp8266.local/led1?state=off")
+        self.speak_dialog("lights.off")
+        
+    def stop(self):
+        pass
+        
+def create_skill():
+    return Esp8266Skill()
